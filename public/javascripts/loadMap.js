3569aa0fa8b7028b382c0c6976551376cc8b5b59
Özkan Isik <oezkani@icloud.com>
2017-06-12 20:33:45 +0200

    added browserify (not working yet)

--- public/javascripts/loadMap.js
+++ public/javascripts/loadMap.js
@@ -1,6 +1,6 @@
 // initialize the map on the "map" div with a given center and zoom
 var map = L.map('mapid', {
-    center: [51.505, -0.09],
+    center: [49.750037, 6.637174],
     zoom: 13
 });
 
@@ -9,10 +9,12 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'ba
 
 // create a red polyline from an array of LatLng points
 var latlngs = [
-    [45.51, -122.68],
-    [37.77, -122.43],
-    [34.04, -118.2]
+	[49.75, 6.64],
+	[49.76, 6.64],
+	[49.77, 6.65],
 ];
+
+
 var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
 // zoom the map to the polyline
 map.fitBounds(polyline.getBounds());
